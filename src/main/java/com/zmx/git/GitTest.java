575217e403d7ce54d8cf7b356e020dfc21dec5c2
Marcin_glb <goodMorning_glb@zmx.com>
2021-05-04 21:57:14 +0800

    push commit

--- src/main/java/com/zmx/git/GitTest.java
+++ src/main/java/com/zmx/git/GitTest.java
@@ -7,6 +7,7 @@ public class GitTest {
         System.out.println("hello Git3!");
         System.out.println("hot_fix test!");
         System.out.println("master test!");
+        System.out.println("push test!");
 
     }
 }
